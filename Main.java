db27eb3566dd3cfca06e7ad1c6d39839deeaf826
SitaCicirko <108766560+SitaCicirko@users.noreply.github.com>
2023-01-14 00:25:21 +0000

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,15 @@
+import java.io.InputStream;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter two numers: ");
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        System.out.println(a + b);
+
+        
+    }
+    
+}
